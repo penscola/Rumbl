fbd805239a6b3a714c1cf0c4be89aaafd627d051
penscola <penscolashackletonfelix@gmail.com>
2023-06-29 17:15:52 +0300

    chore: Managing registration changeset

--- rumbl/lib/rumbl/accounts.ex
+++ rumbl/lib/rumbl/accounts.ex
@@ -18,6 +18,10 @@ defmodule Rumbl.Accounts do
     Repo.all(User)
   end
 
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
